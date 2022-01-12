5adfb4a45e212341803859b180b034ab00d3a91b
Jonathan Kristanto <kristantojonathan@gmail.com>
2022-01-12 23:21:05 +0700

    feat: Complete Cart Feature

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,6 +6,7 @@ use App\Models\DetailTransaction;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\TransactionHeader;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -13,5 +14,31 @@ class CartController extends Controller
         $cart = TransactionHeader::Where('user_id', $id)->Where('status','cart')->first();
 
         return view('cart',['cart'=>$cart]);
+    }
+
+    public function deleteFromCart($product_id, $transaction_id){
+        $stockDeleted = DetailTransaction::Where('transaction_id',$transaction_id)->Where('product_id',$product_id)->first()['quantity'];
+        DetailTransaction::Where('transaction_id',$transaction_id)->Where('product_id',$product_id)->delete();
+
+        $stockOriginal = Product::Where('product_id',$product_id)->first()['product_stock'];
+        Product::Where('product_id',$product_id)->update(['product_stock'=>$stockOriginal+$stockDeleted]);
+
+        $isNull = DetailTransaction::Where('transaction_id',$transaction_id)->first();
+        if($isNull == null){
+            TransactionHeader::Where('transaction_id', $transaction_id)->Where('user_id',Auth::user()->id)->Where('status','cart')->delete();
+        }
+
+        return redirect()->back();
+    }
+
+    public function checkoutTransaction($transaction_id){
+        date_default_timezone_set('Asia/Jakarta');
+        $date = date('Y-m-d H:i:s');
+
+        TransactionHeader::Where('transaction_id',$transaction_id)->Where('user_id',Auth::user()->id)
+            ->where('status','cart')->update(['status'=>'purchased','transaction_time'=>$date]);
+        $trans = TransactionHeader::Where('user_id',Auth::user()->id)->where('status','purchased')->get();
+
+        return view('transaction',['trans'=>$trans]);
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,29 +2,74 @@
 
 @section('content')
     <div class="container">
+        <h3 class="my-2">Shopping Cart</h3>
+        <?php
+        $number = 1;
+        $grandTotal = 0;
+        ?>
         <table class="table mt-2">
             <thead class="thead-light">
                 <tr>
-                    <th scope="col" class="userid">Book ID</th>
-                    <th scope="col">Book Title</th>
-                    <th scope="col" class="username">Quantity</th>
+                    <th scope="col" class="userid">ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Sub-Total</th>
+                    <th scope="col">Remove</th>
                 </tr>
             </thead>
             <tbody>
                 @if ($cart == null)
                     <tr>
-                        <td colspan="3"><h2>There are no item in the cart</h2></td>
+                        <td colspan="3">
+                            <h2>There are no item in the cart</h2>
+                        </td>
                     </tr>
                 @else
                     @foreach ($cart->detailTransaction as $crt)
-                    <tr>
-                        <td class="userid">{{ $crt->product->product_id }}</td>
-                        <td>{{ $crt->product->product_title }}</td>
-                        <td class="username">{{ $crt->quantity }}</td>
-                    </tr>
+                        <tr>
+                            <td>
+                                {{ $number }}
+                            </td>
+                            <td>
+                                {{ $crt->product->product_title }}
+                            </td>
+                            <td>
+                                {{ $crt->product->product_price }}
+                            </td>
+                            <td>
+                                {{ $crt->quantity }}
+                            </td>
+                            <td>
+                                {{ $crt->quantity * $crt->product->product_price }}
+                            </td>
+                            <td>
+                                <form action="/delete-item-cart/{{ $crt->product_id }}/{{ $crt->transaction_id }}"
+                                    method="POST">
+                                    {{ method_field('DELETE') }}
+                                    @csrf
+                                    <button type='submit' class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        <?php
+                        $number += 1;
+                        $grandTotal += $crt->quantity * $crt->product->product_price;
+                        ?>
                     @endforeach
                 @endif
             </tbody>
         </table>
+        @if ($cart != null)
+            <div class="d-flex flex-row-reverse">
+                <p>Grand Total: Rp.{{ $grandTotal }},-</p>
+            </div>
+            <div class="d-flex flex-row-reverse">
+                <form action="/checkout/{{ $crt->transaction_id }}" method="POST">
+                    @csrf
+                    <button type='submit' class="btn btn-primary">Checkout</button>
+                </form>
+            </div>
+        @endif
     </div>
 @endsection
